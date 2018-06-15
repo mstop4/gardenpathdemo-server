4dea1e64a86c322419275df62170ba303bc7a777
mstop4 <mstop4@gmail.com>
2018-06-14 22:58:46 -0400

    added seed (json)

--- src/routes/seed.js
+++ src/routes/seed.js
@@ -2,11 +2,19 @@ import express from 'express'
 import randomWords from 'random-words'
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  const words = randomWords(5)
-  res.status(200).render('pages/seed', {
-    data: words
-  })
+router.get('/:format', (req, res) => {
+  const words = randomWords(parseInt(req.query.limit) || 5)
+
+  if (req.params.format === 'html') {
+    res.status(200).render('pages/seed', {
+      data: words
+    })
+  } else if (req.params.format === 'json') {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(JSON.stringify(words))
+  } else {
+    res.status(400).send('Error: Unknown format')
+  }
 })
 
 module.exports = router
